a9e4450521c1cb0c79bff46408b164214a9fe9ab
Kunal Kashyap <kashyapkunal142@gmail.com>
2025-01-30 19:41:22 +0530

    update mapper for employee

--- hadoop/3-employee/mapper.java
+++ hadoop/3-employee/mapper.java
@@ -7,15 +7,11 @@ import org.apache.hadoop.mapred.*;
 
 public class mapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, DoubleWritable> {
 	public void map(LongWritable key, Text value, OutputCollector<Text, DoubleWritable> output, Reporter r) throws IOException {
-		
-		String[] line = value.toString().split("\\t");
-			
-		if(line.length < 9) {
-			return;
-		}
-		
-		Double salary = Double.parseDouble(line[8]);
-		
+
+		String[] line = value.toString().split(",");
+
+		Double salary = Double.parseDouble(line[4]);
+
 		output.collect(new Text(line[3]), new DoubleWritable(salary));
 	}
 }
--- hadoop/3-employee/reducer.java
+++ hadoop/3-employee/reducer.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.mapred.*;
 
 public class reducer extends MapReduceBase implements Reducer<Text, DoubleWritable, Text, DoubleWritable> {
 	public void reduce(Text key, Iterator<DoubleWritable> value, OutputCollector<Text, DoubleWritable> output, Reporter r) throws IOException {
-		
+
 		int count = 0;
 		double sum = 0.0;
 
@@ -15,9 +15,9 @@ public class reducer extends MapReduceBase implements Reducer<Text, DoubleWritab
 			count++;
 			sum += value.next().get();
 		}
-		
-		output.collect(new Text(key + " Average"), new DoubleWritable(sum / count));
-		
+
+		output.collect(new Text(key + " Average Salary"), new DoubleWritable(sum / count));
+
 		output.collect(new Text(key + " Count"), new DoubleWritable(count));
 	}
 }
